c6e5df03b586d69c0abc1ca22e09bd54eff35a07
Maxime Lambert <lambert.maxime@protonmail.com>
2026-05-13 15:36:02 +0200

    feat: Added tanstack for efficient query state handling

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,19 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { HelmetProvider } from 'react-helmet-async'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import './index.css'
 import './i18n'
 import AppRouter from './Router'
 
+const queryClient = new QueryClient()
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <AppRouter />
+    <HelmetProvider>
+      <QueryClientProvider client={queryClient}>
+        <AppRouter />
+      </QueryClientProvider>
+    </HelmetProvider>
   </StrictMode>
 )
